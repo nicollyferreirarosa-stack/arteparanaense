eb1a0a1c88bddb0b02c0adac7b503d908be75daa
nicollyferreirarosa-stack <nicolly.ferreira.rosa@escola.pr.gov.br>
2026-09-16 17:38:26 -0300

    mudei o script o index e o stely

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const botao = document.querySelector ("#botaomensagem");
-const mensagem = document.querySelector("#mensagemInterativa");
-botao.addEventListener("click, mostrar mensagem")
-function mostrarMensagem() {
-    mensagem.textContent =
-    "Você acabou de criar uma interação com javaScript"
-
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir)
+function curtir(){
+    const contador = botaoCurtir.querySelector("span");
+      if(curtiu === false){
+        contador.textContent++;
+     curtiu = true;
+    }
+     else{
+        contador.textContent--;
+        curtiu = false;
+     }
 }
+});
