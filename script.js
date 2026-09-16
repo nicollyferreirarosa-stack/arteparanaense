c222a282a24cc83357929944d3a7b5cb1dc0ac61
nicollyferreirarosa-stack <nicolly.ferreira.rosa@escola.pr.gov.br>
2026-09-16 17:02:31 -0300

    mudei o script e o index

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const botao = document.querySelector ("#botaomensagem");
+const mensagem = document.querySelector("#mensagemInterativa");
+botao.addEventListener("click, mostrar mensagem")
+function mostrarMensagem() {
+    mensagem.textContent =
+    "Você acabou de criar uma interação com javaScript"
 
+}
